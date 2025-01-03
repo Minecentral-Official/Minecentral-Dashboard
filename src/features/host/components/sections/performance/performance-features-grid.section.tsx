7003a8aafaae841b52879b8b34841f7d4d7bbc66
Hugo Hsi <hugohsidev@gmail.com>
2025-01-03 00:11:04 -0500

    adds copy to sepcs section on performance page

--- src/features/host/components/sections/performance/performance-features-grid.section.tsx
+++ src/features/host/components/sections/performance/performance-features-grid.section.tsx
@@ -1,4 +1,13 @@
-import { User } from 'lucide-react';
+import {
+  ChartArea,
+  ChevronsUp,
+  Cpu,
+  Gamepad2,
+  Settings,
+  Shield,
+  Volleyball,
+  Wrench,
+} from 'lucide-react';
 
 import SectionWrapper from '@/components/sections/primitives/section.wrapper';
 import { Badge } from '@/components/ui/badge';
@@ -9,102 +18,107 @@ export default function PerformanceFeaturesGridSection() {
       <div className='flex flex-col gap-10'>
         <div className='flex flex-col items-start gap-4'>
           <div>
-            <Badge>Platform</Badge>
+            <Badge>Unmatched Speed</Badge>
           </div>
           <div className='flex flex-col gap-2'>
             <h2 className='font-regular max-w-xl text-left text-3xl tracking-tighter md:text-5xl'>
-              Something new!
+              Performance That Powers Your World
             </h2>
             <p className='max-w-xl text-left text-lg leading-relaxed tracking-tight text-muted-foreground lg:max-w-lg'>
-              Managing a small business today is already tough.
+              Explore the powerful specs behind our lag-free experience.
             </p>
           </div>
         </div>
         <div className='flex flex-col gap-8 sm:grid sm:grid-cols-2 lg:grid lg:grid-cols-3 xl:grid-cols-4'>
           <div className='flex aspect-square h-full w-full flex-col justify-between rounded-md bg-muted p-6 lg:col-span-2 lg:row-span-2'>
-            <User className='h-8 w-8 stroke-1' />
+            <Cpu className='h-8 w-8 stroke-1' />
             <div className='flex flex-col'>
-              <h3 className='text-xl tracking-tight'>Pay supplier invoices</h3>
+              <h3 className='text-xl tracking-tight'>Powered by AMD Ryzen</h3>
               <p className='max-w-xs text-base text-muted-foreground'>
-                Our goal is to streamline SMB trade, making it easier and faster
-                than ever.
+                Experience unmatched performance with our servers equipped with
+                the cutting-edge AMD Ryzen [model name], delivering the ultimate
+                gaming and hosting experience.
               </p>
             </div>
           </div>
 
           <div className='flex aspect-square h-full flex-col justify-between rounded-md bg-muted p-6'>
-            <User className='h-8 w-8 stroke-1' />
+            <Settings className='h-8 w-8 stroke-1' />
             <div className='flex flex-col'>
-              <h3 className='text-xl tracking-tight'>Pay supplier invoices</h3>
+              <h3 className='text-xl tracking-tight'>16 Cores of Power</h3>
               <p className='max-w-xs text-base text-muted-foreground'>
-                Our goal is to streamline SMB trade, making it easier and faster
-                than ever.
+                Ensure smooth, lag-free Minecraft gameplay powered by 16 CPU
+                cores
               </p>
             </div>
           </div>
 
           <div className='flex aspect-square h-full flex-col justify-between rounded-md bg-muted p-6'>
-            <User className='h-8 w-8 stroke-1' />
+            <Volleyball className='h-8 w-8 stroke-1' />
             <div className='flex flex-col'>
-              <h3 className='text-xl tracking-tight'>Pay supplier invoices</h3>
+              <h3 className='text-xl tracking-tight'>[Thread Count] Threads</h3>
               <p className='max-w-xs text-base text-muted-foreground'>
-                Our goal is to streamline SMB trade, making it easier and faster
-                than ever.
+                [Thread count] threads for peak server performance.
               </p>
             </div>
           </div>
 
           <div className='flex aspect-square h-full flex-col justify-between rounded-md bg-muted p-6'>
-            <User className='h-8 w-8 stroke-1' />
+            <ChevronsUp className='h-8 w-8 stroke-1' />
             <div className='flex flex-col'>
-              <h3 className='text-xl tracking-tight'>Pay supplier invoices</h3>
+              <h3 className='text-xl tracking-tight'>
+                Boost Clock Speeds up to [Speed] GHz
+              </h3>
               <p className='max-w-xs text-base text-muted-foreground'>
-                Our goal is to streamline SMB trade, making it easier and faster
-                than ever.
+                Our servers ramp up with boost clock speeds of up to [speed]
+                GHz, delivering smooth, lag-free gameplay.
               </p>
             </div>
           </div>
 
           <div className='flex aspect-square h-full flex-col justify-between rounded-md bg-muted p-6'>
-            <User className='h-8 w-8 stroke-1' />
+            <Shield className='h-8 w-8 stroke-1' />
             <div className='flex flex-col'>
-              <h3 className='text-xl tracking-tight'>Pay supplier invoices</h3>
+              <h3 className='text-xl tracking-tight'>DDoS Protection</h3>
               <p className='max-w-xs text-base text-muted-foreground'>
-                Our goal is to streamline SMB trade, making it easier and faster
-                than ever.
+                Safeguard your server from attacks with our industry-leading
+                DDoS protection system.
               </p>
             </div>
           </div>
 
           <div className='flex aspect-square h-full flex-col justify-between rounded-md bg-muted p-6'>
-            <User className='h-8 w-8 stroke-1' />
+            <Wrench className='h-8 w-8 stroke-1' />
             <div className='flex flex-col'>
-              <h3 className='text-xl tracking-tight'>Pay supplier invoices</h3>
+              <h3 className='text-xl tracking-tight'>
+                Optimized for Minecraft
+              </h3>
               <p className='max-w-xs text-base text-muted-foreground'>
-                Our goal is to streamline SMB trade, making it easier and faster
-                than ever.
+                Servers fine-tuned for the best Minecraft experience.
               </p>
             </div>
           </div>
 
           <div className='flex aspect-square h-full flex-col justify-between rounded-md bg-muted p-6'>
-            <User className='h-8 w-8 stroke-1' />
+            <Gamepad2 className='h-8 w-8 stroke-1' />
             <div className='flex flex-col'>
-              <h3 className='text-xl tracking-tight'>Pay supplier invoices</h3>
+              <h3 className='text-xl tracking-tight'>
+                Customizable Server Specs
+              </h3>
               <p className='max-w-xs text-base text-muted-foreground'>
-                Our goal is to streamline SMB trade, making it easier and faster
-                than ever.
+                Fine-tune your server&apos;s RAM and storage to match your
+                gameplay needs.
               </p>
             </div>
           </div>
 
           <div className='flex h-full flex-col justify-between rounded-md bg-muted p-6 lg:col-span-2'>
-            <User className='h-8 w-8 stroke-1' />
+            <ChartArea className='h-8 w-8 stroke-1' />
             <div className='flex flex-col'>
-              <h3 className='text-xl tracking-tight'>Pay supplier invoices</h3>
+              <h3 className='text-xl tracking-tight'>Real-Time Monitoring</h3>
               <p className='max-w-xs text-base text-muted-foreground'>
-                Our goal is to streamline SMB trade, making it easier and faster
-                than ever.
+                Track server performance metrics, including CPU usage, RAM, and
+                network speeds, all in real time.
               </p>
             </div>
           </div>
