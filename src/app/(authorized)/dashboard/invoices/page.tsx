44503468790fbf851c0b4c800b8092568c9b37bd
Hugo Hsi <hugohsidev@gmail.com>
2025-01-14 15:19:08 -0500

    adds invoice table to invoices page

--- src/app/(authorized)/dashboard/invoices/page.tsx
+++ src/app/(authorized)/dashboard/invoices/page.tsx
@@ -1,3 +1,19 @@
+import { Check, X } from 'lucide-react';
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
 import {
   hostStripeGetInvoices,
   TPurchaseInvoiceData,
@@ -8,29 +24,63 @@ export default async function GeneralInvoicesPage() {
   const invoices = await hostStripeGetInvoices();
   if (!invoices) return <>No Invoices</>;
   return (
-    <>
-      {invoices.map((invoice) => (
-        <InvoiceRow invoice={invoice} key={invoice.id} />
-      ))}
-    </>
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Invoices</TableHead>
+          <TableHead>Amount Due</TableHead>
+
+          <TableHead>Invoice Created</TableHead>
+          <TableHead>Invoices</TableHead>
+          <TableHead className='text-end'>Paid</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {invoices.map((invoice) => (
+          <InvoiceRow {...invoice} key={invoice.id} />
+        ))}
+      </TableBody>
+    </Table>
   );
 }
 
 function InvoiceRow({
-  invoice: {
-    //UNCOMMENT when you use this component
-    // amount_due,
-    // amount_paid,
-    // amount_remaining,
-    // created,
-    // due_date,
-    // id,
-    // paid,
-    // status,
-  },
-}: {
-  invoice: TPurchaseInvoiceData;
-}) {
+  // UNCOMMENT when you use this component
+  amount_due,
+  // amount_paid,
+  // amount_remaining,
+  created,
+  due_date,
+  id,
+  paid,
+  status,
+}: TPurchaseInvoiceData) {
   //Create a Row thing here
-  return <></>;
+  return (
+    <TableRow>
+      <TableCell className='font-medium'>{id}</TableCell>
+      <TableCell>$ {amount_due / 100}</TableCell>
+      {/* ALAIN: What does this date number represent? I tried formatting it and it gave me a date in the 1970s LOL */}
+      <TableCell>{new Date(created).toLocaleDateString('en-US')}</TableCell>
+      <TableCell>
+        {due_date ? new Date(created).toLocaleDateString('en-US') : 'N/A'}
+      </TableCell>
+      <TableCell className='text-end'>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              {paid ?
+                <Check className='mr-1 h-4 w-4' />
+              : <X className='mr-1 h-4 w-4' />}
+            </TooltipTrigger>
+            <TooltipContent>
+              <div>
+                {status && status.charAt(0).toUpperCase() + status.slice(1)}
+              </div>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </TableCell>
+    </TableRow>
+  );
 }
