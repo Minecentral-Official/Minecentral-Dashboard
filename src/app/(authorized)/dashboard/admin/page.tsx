ddb353c5b05247965248263ac927e9621a1b8014
RonanCraft <SuperRonanCraft@gmail.com>
2025-02-09 19:54:24 -0500

    disabled self hack route

--- src/app/(authorized)/dashboard/admin/page.tsx
+++ src/app/(authorized)/dashboard/admin/page.tsx
@@ -1,6 +1,3 @@
-import { setAdmin } from '@/lib/auth/setadmin';
-
-export default async function Page() {
-  await setAdmin();
-  return <>Setting as admin</>;
+export default function Page() {
+  return <>Coming soon</>;
 }
