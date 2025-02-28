4ca0d740f50486e0a68e36a9f65ebe3b2b991e52
RonanCraft <SuperRonanCraft@gmail.com>
2025-02-28 01:09:58 -0500

    eslint unused fix

--- src/lib/types/project.type.ts
+++ src/lib/types/project.type.ts
@@ -1,4 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const CategoriesProjects = [
   'plugin',
   'mod',
@@ -8,11 +7,3 @@ export const CategoriesProjects = [
 ] as const;
 
 export type T_ProjectCategory = (typeof CategoriesProjects)[number];
-
-// export type PluginDataBasic = typeof resourceTable.$inferSelect & {
-//   user: typeof user.$inferSelect;
-// };
-
-// export type PluginData_All = PluginDataBasic & {
-//   releases: (typeof resourceReleaseTable.$inferSelect)[];
-// };
